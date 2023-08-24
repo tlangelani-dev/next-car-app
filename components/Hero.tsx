d32b0aa80d2270e970cd1da375956cb80bc6e588
Tlangelani Mboweni <mbowenit@gmail.com>
2023-08-24 16:55:19 +0200

    update layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,6 +22,12 @@ const Hero = () => {
                     handleClick={handleScroll}
                 />
             </div>
+            <div className="hero__image-container">
+                <div className="hero__image">
+                    <Image src="/hero.png" alt="Hero" fill className="object-contain" />
+                    <div className="hero__image-overlay"></div>
+                </div>
+            </div>
         </div>
     )
 }
